4adc4638da8f63a9e1a488eac9c42f1a0aeb104d
Chen-shin, Yang <yangchenshin77@gmail.com>
2018-10-17 11:15:29 +0000

    完善假資料填充

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UsersSeeder::class);
+        $this->call(UserAddressesSeeder::class);
+        $this->call(ProductsSeeder::class);
+        $this->call(OrdersSeeder::class);
     }
 }
---
+++ database/seeds/UserAddressesSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Models\User;
+use App\Models\UserAddress;
+use Illuminate\Database\Seeder;
+
+class UserAddressesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::all()->each(function (User $user) {
+            factory(UserAddress::class, random_int(1, 3))->create(['user_id' => $user->id]);
+        });
+    }
+}
