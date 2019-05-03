855e597746bcc03a0a7cc09558ea74fdbcb878cf
Lucas Yang <yangchenshin77@gmail.com>
2019-05-03 13:35:12 +0800

    Update page footer links

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -2,11 +2,11 @@
   <div class="container">
     <div class="row py-3">
       <div class="col-lg">
-        由 Chen-shin, Yang 設計和編碼
+        由 <a href="https://github.com/ycs77" target="_blank">Lucas Yang</a> 設計和編碼
       </div>
 
       <div class="col-lg text-lg-right">
-        <a href="mailto:yangchensgin77@gmail.com">聯絡我們</a>
+        <a href="https://github.com/ycs77/Laravel-shop" target="_blank">商城原始碼</a>
       </div>
     </div>
   </div>
