b92f738453bccc9381ebba3dc1ae70ce7ca6590f
Lucas Yang <yangchenshin77@gmail.com>
2020-01-07 16:37:27 +0800

    Revert admin home dashboard

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -12,26 +12,23 @@ class HomeController extends Controller
 {
     public function index(Content $content)
     {
-        // return $content
-        //     ->header('Dashboard')
-        //     ->description('Description...')
-        //     ->row(Dashboard::title())
-        //     ->row(function (Row $row) {
-
-        //         $row->column(4, function (Column $column) {
-        //             $column->append(Dashboard::environment());
-        //         });
-
-        //         $row->column(4, function (Column $column) {
-        //             $column->append(Dashboard::extensions());
-        //         });
-
-        //         $row->column(4, function (Column $column) {
-        //             $column->append(Dashboard::dependencies());
-        //         });
-        //     });
-
         return $content
-            ->header('儀表板');
+            ->header('儀表板')
+            ->description('description')
+            ->row(Dashboard::title())
+            ->row(function (Row $row) {
+
+                $row->column(4, function (Column $column) {
+                    $column->append(Dashboard::environment());
+                });
+
+                $row->column(4, function (Column $column) {
+                    $column->append(Dashboard::extensions());
+                });
+
+                $row->column(4, function (Column $column) {
+                    $column->append(Dashboard::dependencies());
+                });
+            });
     }
 }
